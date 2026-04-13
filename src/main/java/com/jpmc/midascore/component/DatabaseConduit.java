30a34d826c33dea0e82f5100db05ce4bd2049f30
unknown <kofi.wiredu21@my.northampton.ac.uk>
2026-04-13 22:47:26 +0100

    Completed Task 3: Integrated H2 database and transaction validation logic

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,4 +1,4 @@
-package com.jpmc.midascore.component;
+/* package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.repository.UserRepository;
@@ -16,4 +16,62 @@ public class DatabaseConduit {
         userRepository.save(userRecord);
     }
 
+} */
+
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRepository;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRepository;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class DatabaseConduit {
+    private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
+
+    public DatabaseConduit(UserRepository userRepository, TransactionRepository transactionRepository) {
+        this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
+    // METHOD 1: For the Test (UserPopulator) to save initial users
+    public void save(UserRecord userRecord) {
+        userRepository.save(userRecord);
+    }
+
+    // METHOD 2: For the KafkaConsumer to process transactions
+    @Transactional
+    public void save(Transaction transaction) {
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+
+        if (sender != null && recipient != null && sender.getBalance() >= transaction.getAmount()) {
+            sender.setBalance(sender.getBalance() - transaction.getAmount());
+            recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+            userRepository.save(sender);
+            userRepository.save(recipient);
+            transactionRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount()));
+
+            System.out.println(userRepository.findAll());
+
+            userRepository.findAll().forEach(user -> {
+                if (user.getName().equals("waldorf")) {
+                    System.out.println("FINAL WALDORF BALANCE >> " + user.getBalance());
+                }
+            });
+        }
+    }
 }
--- src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
+++ src/main/java/com/jpmc/midascore/component/KafkaConsumer.java
@@ -1,15 +1,27 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Component
 public class KafkaConsumer {
 
+    private final DatabaseConduit databaseConduit;
+
+    public KafkaConsumer(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
+
     @KafkaListener(topics = "${general.kafka-topic}")
     public void listen(Transaction transaction) {
-        // You'll see this in your console when it works
-        System.out.println("DEBUG - Received: " + transaction);
+        databaseConduit.save(transaction);
     }
+
 }
